f7d7a245f6d04f0924a9ba86ce9ccc72eebeccbf
Meier-Stefan <smeier01@student.bbk.ac.uk>
2024-11-05 14:06:52 +0000

    docs: add comments as requested by the university

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
+// Tell the linter that prettier is installed as formatter. More about the difference between linter and formatter here: https://prettier.io/docs/en/comparison
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
