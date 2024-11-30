b1c34bfdf82fd61d875fd51b4c08aacb66e8c401
Meier-Stefan <smeier01@student.bbk.ac.uk>
2024-11-30 16:26:06 +0000

    feat: connect to database

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 import express from "express";
+import { connect } from "mongoose";
+import { config } from "dotenv";
+
 import { postRouter } from "#routes/post/index.js";
 import { topicRouter } from "#routes/topic/index.js";
 import { userRouter } from "#routes/user/index.js";
+
+config({ path: `.env.${process.env.NODE_ENV}` });
+
+const uri = process.env.MONGODB_URI;
+
+const main = async () => {
+  console.log(`Connecting to DB`);
+  await connect(uri);
+  console.log(`Connected to DB`);
+};
 
 const app = express();
 
@@ -11,5 +24,7 @@ app.get("/", (req, res) => {
 app.use("/post", postRouter);
 app.use("/topic", topicRouter);
 app.use("/user", userRouter);
+
+main().catch((error) => console.log(error));
 
 app.listen(3000);
