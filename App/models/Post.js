664052f84ae716e1b153657725c6b1a8360479b8
Meier-Stefan <smeier01@student.bbk.ac.uk>
2024-12-01 14:03:33 +0000

    feat: create models

---
+++ App/models/Post.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const PostSchema = Schema({
+  title: { type: String, required: true, min: 3, max: 256 },
+  text: { type: String, required: true, min: 3, max: 2048 },
+  authorId: { type: Schema.Types.ObjectId, required: true, ref: "users" },
+  topic: [{ type: Schema.Types.ObjectId, required: true, ref: "topics" }],
+  comments: [
+    {
+      commentorId: { type: Schema.Types.ObjectId, ref: "users" },
+      text: { type: String, required: true, min: 3, max: 1024 },
+      date: { type: Date, default: Date.now },
+    },
+  ],
+  likes: [{ type: Schema.Types.ObjectId, ref: "users" }],
+  dislikes: [{ type: Schema.Types.ObjectId, ref: "users" }],
+  date: { type: Date, default: Date.now },
+});
+
+const Post = model("posts", PostSchema);
+
+export { Post };
---
+++ App/models/Topic.js
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose";
+
+const TopicSchema = Schema({
+  title: { type: String, required: true, min: 3, max: 256 },
+  posts: [{ type: Schema.Types.ObjectId, required: true, ref: "posts" }],
+});
+
+const Topic = model("posts", TopicSchema);
+
+export { Topic };
