b4937d2724069b686a28258a627846186cf1f485
Meier-Stefan <smeier01@student.bbk.ac.uk>
2024-12-01 20:29:09 +0000

    feat: user can get all posts of topic

--- App/controllers/topic/detail.js
+++ App/controllers/topic/detail.js
@@ -1,5 +1,13 @@
-const topicDetailController = (req, res) => {
-  res.send("Topic detail endpoint");
+import { Post } from "#models/Post.js";
+
+const topicDetailController = async (req, res) => {
+  const topicId = req.params.id;
+  try {
+    const postsToGet = await Post.find({ topic: { $in: topicId } });
+    res.send(postsToGet);
+  } catch (error) {
+    res.send({ message: error.message });
+  }
 };
 
 export { topicDetailController };
