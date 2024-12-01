8984813a0bf937bc145c66f458ecd8c7b38e0a57
Meier-Stefan <smeier01@student.bbk.ac.uk>
2024-12-01 17:33:23 +0000

    feat: user can get list of newest posts

--- App/controllers/post/list.js
+++ App/controllers/post/list.js
@@ -1,5 +1,12 @@
-const postListController = (req, res) => {
-  res.send("Post list endpoint");
+import { Post } from "#models/Post.js";
+
+const postListController = async (req, res) => {
+  try {
+    const postsToGet = await Post.find().limit(15);
+    res.send(postsToGet);
+  } catch (error) {
+    res.send({ message: error.message });
+  }
 };
 
 export { postListController };
