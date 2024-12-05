c6a6f6210b4feb7c4afa161855aa80a768309f54
Meier-Stefan <smeier01@student.bbk.ac.uk>
2024-12-05 14:00:53 +0000

    feat: user gets informed if post is inactive when action is attempted

--- App/routes/post/index.js
+++ App/routes/post/index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import * as pc from "#controllers/post/index.js";
 import { isAuthenticated } from "#middleware/authentication.js";
+import { isPostsActive } from "#middleware/isPostActive.js";
 
 const postRouter = express.Router();
 
 postRouter.get("/", isAuthenticated, pc.showPostList);
 postRouter.post("/new", isAuthenticated, pc.createPostController);
 postRouter.get("/:id", isAuthenticated, pc.showPostDetail);
-postRouter.post("/:id/comment", isAuthenticated, pc.commentOnPost);
-postRouter.post("/:id/react", isAuthenticated, pc.reactToPost);
+postRouter.post(
+  "/:id/comment",
+  isAuthenticated,
+  isPostsActive,
+  pc.commentOnPost,
+);
+postRouter.post("/:id/react", isAuthenticated, isPostsActive, pc.reactToPost);
 
 export { postRouter };
